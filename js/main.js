9209d161eb94faf6084266ad27f051657e54c2eb
Victor Raul <v1c70rotakmer@gmail.com>
2023-06-01 11:23:06 -0500

    Terminar seccion portafolio

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ window.addEventListener("scroll",function(){
     menu.classList.remove("fa-times");
     header.classList.remove("toggle");
 })
+
+document.addEventListener("DOMContentLoaded", function(){
+    crearGaleria();
+});
+
+function crearGaleria(){
+    const galeria = document.querySelector(".portfolio .box-container");
+    for(let i=1; i<=12;i++){
+        const imagen=document.createElement("IMG");
+        imagen.src=`../img/${i}.png`;
+        imagen.dataset.imagenId=i;
+
+
+        // Crear nodo lista y agregar img
+        const lista = document.createElement("LI");
+        lista.classList.add("box")
+        lista.appendChild(imagen);
+        galeria.appendChild(lista);
+    }
+}
